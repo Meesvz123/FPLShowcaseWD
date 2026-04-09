28eb09951bc242e8f51e4bd31d84f0e91125ca62
Meesvz <s1213648@student.windesheim.nl>
2026-04-09 15:07:37 +0200

    Add project files.

---
+++ FPL Showcase WD/Models/Player.cs
@@ -0,0 +1,19 @@
+﻿namespace FPL_Showcase_WD.Models
+{
+    public class Player
+    {
+        public int Id { get; set; }
+
+        public string Naam { get; set; }
+
+        public string Positie { get; set; }
+
+        public string Club { get; set; }
+
+        public int Prijs { get; set; }
+
+        public int Statistieken { get; set; }
+
+
+    }
+}
---
+++ FPL Showcase WD/Models/Wedstrijden.cs
@@ -0,0 +1,17 @@
+﻿namespace FPL_Showcase_WD.Models
+{
+    public class Wedstrijden
+    {
+        public int Id { get; set; }
+        public string Thuisclub { get; set; }
+        public int UitClub { get; set; }
+
+        public DateTime Datum { get; set; }
+
+        public string Uitslag { get; set; }
+
+        public string Statistieken { get; set; }
+
+
+    }
+}
